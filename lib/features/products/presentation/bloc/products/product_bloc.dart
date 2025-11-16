281c82f4dd7222dea41a01f0160163e7845b8234
MualifUlilM <mualifmisbakh2017@gmail.com>
2025-11-16 23:07:19 +0700

    feat(products): enhance product loading and searching functionality with improved logging and state management

--- lib/features/products/presentation/bloc/products/product_bloc.dart
+++ lib/features/products/presentation/bloc/products/product_bloc.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'dart:developer' as Developer;
+import 'dart:developer' as Dev;
 
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
@@ -13,6 +14,7 @@ part 'product_state.dart';
 class ProductBloc extends Bloc<ProductEvent, ProductState> {
   final ApiService apiService;
   Timer? _debounce;
+  List<ProductModel> _loadedProducts = [];
   List<ProductModel> _allProducts = [];
   bool isSearching = false;
   final int _productLimit = 5;
@@ -25,10 +27,8 @@ class ProductBloc extends Bloc<ProductEvent, ProductState> {
       );
 
       result.fold((error) => emit(ProductError(message: error)), (data) {
+        _loadedProducts = data;
         final hasReachedMax = data.length < _productLimit;
-        Developer.log(
-          'Fetched products: ${data.length}, hasReachedMax: $hasReachedMax',
-        );
         emit(ProductLoaded(listProduct: data, hasReachedMax: hasReachedMax));
       });
     });
@@ -40,12 +40,8 @@ class ProductBloc extends Bloc<ProductEvent, ProductState> {
           limit: _productLimit,
           offset: currentOffset,
         );
-        Developer.log('Fetching more products at offset $result');
 
         result.fold((error) => emit(ProductError(message: error)), (data) {
-          Developer.log(
-            'Fetched more products: ${data.length} at offset $currentOffset',
-          );
           if (data.isEmpty) {
             emit(currentState.copyWith(hasReachedMax: true));
           } else {
@@ -61,12 +57,20 @@ class ProductBloc extends Bloc<ProductEvent, ProductState> {
     });
     on<SearchProduct>((event, emit) async {
       emit(ProductLoading());
-      isSearching = event.keyword.isNotEmpty;
       _debounce?.cancel();
+      isSearching = event.keyword.isNotEmpty;
       _debounce = Timer(const Duration(milliseconds: 300), () async {
         if (event.keyword.isEmpty) {
-          add(SearchResult(results: _allProducts));
+          Dev.log('Clearing search, loading all products');
+          Dev.log('All products: ${_loadedProducts.length}');
+          add(SearchResult(results: _loadedProducts));
         } else {
+          if (_allProducts.isEmpty) {
+            final result = await apiService.getAllProducts();
+            result.fold((l) => ProductError(message: l), (data) {
+              _allProducts = data;
+            });
+          }
           final filtered = _allProducts
               .where(
                 (item) => item.title.toLowerCase().contains(
@@ -74,6 +78,7 @@ class ProductBloc extends Bloc<ProductEvent, ProductState> {
                 ),
               )
               .toList();
+          Dev.log('Searching products with keyword: ${filtered}');
           add(SearchResult(results: filtered));
         }
       });
@@ -82,7 +87,7 @@ class ProductBloc extends Bloc<ProductEvent, ProductState> {
       if (event.results.isEmpty) {
         emit(ProductError(message: 'Not found'));
       } else {
-        emit(ProductLoaded(listProduct: event.results));
+        emit(ProductLoaded(listProduct: event.results, hasReachedMax: true));
       }
     });
   }
